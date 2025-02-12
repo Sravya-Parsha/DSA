503841cd3c46ea61af1636a28e055536d16f1a3b
Sravya-Parsha <parshasravya2@gmail.com>
2025-02-12 21:21:42 +0530

    Add files via upload

---
+++ countanum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,count=0;
+	int r,num;
+	scanf("%d",&n);
+	num=n;
+	while(n>0)
+	{
+		r=n%10;
+		if(num%r==0)
+		{
+			count++;
+		}
+		n=n/10;
+	}
+	printf("%d",count);
+}
